af85e1203e5c80f30fc16ad7af19f3568e2b2f47
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-11 13:27:40 +0500

    splash screen updated to stateful

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Splash extends StatelessWidget {
+import 'package:smartchoice/screens/intro/intro.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  startTime() {
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => IntoScreens()));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
