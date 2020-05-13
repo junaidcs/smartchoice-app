ee258aeb1635e71ae7f62a8b9d5927d37152fcab
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-13 17:14:57 +0500

    referrals page

---
+++ lib/screens/referrals/referrals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Referrals extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Referrals screen',
+      style: TextStyle(
+        color: Theme.of(context).primaryColor,
+        fontSize: 26,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
